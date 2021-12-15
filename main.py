c1402408aedc034719d70fe311a75e53409d56b7
Gabe-H <gabe.haarberg@gmail.com>
2021-12-15 09:47:35 -0800

    new packet method

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import socket
 
+
 UDP_IP = "127.0.0.1"
 UDP_PORT = 12000
 
@@ -10,21 +11,11 @@ sock_idle = True
 
 while True:
   data, _addr = sock.recvfrom(1024) # buffer size is 1024 bytes
-  if sock_idle:
-    if data[0] == 36:
-      sock_idle = False
-    else:
-      print(data[0])
-  
-  else:
-    axes = []
-    if (data[0] == 35):
-      sock_idle = True
+  axes = []
+  for i in range(6):
+    raw_axis = data.split(b'&')[i]
+    cmd = raw_axis[0]
+    val = float(raw_axis[1:])
+    axes.append(val)
 
-    else:
-      for i in range(6):
-        msb = data[i*2]
-        lsb = data[(i*2)+1]
-        axes.append((msb << 8) + lsb)
-
-      print(axes)
+  print(axes)
